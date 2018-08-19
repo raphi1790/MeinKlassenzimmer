53e5153d5d918f2816f45358c351649a13eacf7c
raphi1790 <raphael_90@bluewin.ch>
2018-08-19 20:21:03 +0200

    Remove passwords.

--- meinKlassenzimmer/functions/src/dbconnection.js
+++ meinKlassenzimmer/functions/src/dbconnection.js
@@ -1,10 +1,10 @@
 var mysql      = require('mysql');
 
 
-const connectionName = 'meinklassenzimmer-184820:us-central1:meinklassenzimmer-database';
-const dbUser = 'raphael';
-const dbPass = 'CodingIsFun21?';
-const dbName = 'meinklassenzimmerdb';
+const connectionName = '';
+const dbUser = '';
+const dbPass = '';
+const dbName = '';
 
 const pool = mysql.createPool({
   connectionLimit : 1,
