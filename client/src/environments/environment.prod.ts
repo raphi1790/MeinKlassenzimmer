50cf0fb1167af8004e45b72bacb2f2bc876accbd
Raphael Schönenberger <raphael.schoenenberger@gmail.com>
2019-07-17 21:45:49 +0200

    Update environment.prod.ts

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCfwrUqTMbYVbfYjHLetIfJiT6vwA-Y27U",
+    apiKey: "",
     authDomain: "meinklassenzimmer.firebaseapp.com",
     databaseURL: "https://meinklassenzimmer.firebaseio.com",
     projectId: "meinklassenzimmer",
     storageBucket: "meinklassenzimmer.appspot.com",
-    messagingSenderId: "946677335646",
-    appId: "1:946677335646:web:17d2338c354f9858"
+    messagingSenderId: "",
+    appId: ""
   }
   };
   
