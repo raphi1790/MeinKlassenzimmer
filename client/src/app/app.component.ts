ac128eea8241dca16bb0389258905f82ce9bd1ec
Igor Gazivoda <igor.gazivoda@adtrac.ch>
2021-02-21 19:56:45 +0100

    dark mode

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,37 +1,44 @@
-import { Component} from '@angular/core';
-import { AuthService } from './services/auth/auth.service';
-import { User } from './models/user';
-import {environment} from '../environments/environment'
-
-
+import { Component, Inject } from "@angular/core";
+import { AuthService } from "./services/auth/auth.service";
+import { User } from "./models/user";
+import { environment } from "../environments/environment";
+import { DOCUMENT } from "@angular/common";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  isDarkTheme: boolean = false;
 
-  constructor(public auth: AuthService) {
-    console.log(environment.production? "production": "dev")
-    
-      
+  constructor(
+    public auth: AuthService,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    console.log(environment.production ? "production" : "dev");
   }
 
   opened: boolean;
   showTools = false;
   showVerwaltung = false;
 
-
   toggleToolsMenu() {
     this.showTools = !this.showTools;
- }
- toggleVerwaltungMenu(){
-  this.showVerwaltung = !this.showVerwaltung;
- }
+  }
+  toggleVerwaltungMenu() {
+    this.showVerwaltung = !this.showVerwaltung;
+  }
 
-  title = 'app';
+  changeTheme(): void {
+    if (this.isDarkTheme) {
+      this.document.body.classList.remove("dark-theme");
+      this.isDarkTheme = false;
+    } else {
+      this.isDarkTheme = true;
+      this.document.body.classList.add("dark-theme");
+    }
+  }
 
-  
+  title = "app";
 }
-
