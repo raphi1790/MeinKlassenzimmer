afe2e0c7823612ff5552ede970ee49c36a11f0f8
raphi1790 <raphael.schoenenberger@gmail.com>
2025-09-03 22:44:00 +0200

    add qual enviroment

---
+++ client/src/environments/environment.qual.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBVB5VHzTtUtAQ1rwUsamX6kaz-sn_3KdM",
+    authDomain: "meinklassenzimmer-qual.firebaseapp.com",
+    databaseURL: "https://meinklassenzimmer-qual.firebaseio.com",
+    projectId: "meinklassenzimmer-qual",
+    storageBucket: "meinklassenzimmer-qual.firebasestorage.app",
+    messagingSenderId: "435136862828",
+    appId: "1:435136862828:web:9c920859e6b7fbc1ff1649",
+    measurementId: "G-QNGEQ5ZMPC"
+  }
+  };
+  
+
